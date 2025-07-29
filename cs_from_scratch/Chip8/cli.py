c6fb048d5781d874d8c532cc5f6ae30fcae34c6c
S. Co1 <sco1.git@gmail.com>
2025-07-29 19:08:49 +0000

    Init CHIP-8

---
+++ cs_from_scratch/Chip8/cli.py
@@ -0,0 +1,11 @@
+import argparse
+from pathlib import Path
+
+
+def main() -> None:  # noqa: D103
+    parser = argparse.ArgumentParser("Chip8")
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":  # noqa: D103
+    main()
